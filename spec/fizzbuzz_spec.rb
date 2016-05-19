38fffc02c9e16b7ab6ed3d0da127e4ff8f59832b
Luke <lukeaveil@yahoo.com>
2016-05-19 16:24:13 +0100

    third test written

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,13 @@
 require 'fizzbuzz'
 
-#describe 'fizzbuzz' do 
-	#it 'returns "fizz" when passed 3' do
-		#expect(fizzbuzz(3)).to eq 'fizz'
-	#end
-#end
-
 describe 'fizzbuzz' do
 	it 'returns "fizz" when passed 3' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 	end
 	it 'returns "buzz" when passed 5' do
 		expect(fizzbuzz(5)).to eq 'buzz'
+	end
+	it 'returns the number when passed' do
+		expect(fizzbuzz(11)).to eq 11
 	end
 end
